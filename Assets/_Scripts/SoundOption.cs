0c42643328a46534203fd4beddaec6b1400cef35
VirgileMezz <virgile.mezzarobba@laposte.net>
2018-09-24 14:07:56 +0200

    premier Update, menu
    premiere esquisse du menu avec fonctionnalitées moindres

---
+++ Assets/_Scripts/SoundOption.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SoundOption : MonoBehaviour {
+    public AudioSource audio;
+    public Slider slider;
+	public void sliderValueChange()
+    {
+        audio.volume = slider.value;
+    }
+}
---
+++ Assets/_Scripts/UI/UiController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UiController : MonoBehaviour {
+
+	public void loadSceneOnClic(int index)
+    {
+        SceneManager.LoadScene(index, LoadSceneMode.Single);
+    }
+    public void exitOnClic()
+    {
+        Application.Quit();
+    }
+    public void desactivateCanvasOnClic(GameObject o)
+    {
+        o.SetActive(false);
+    }
+    public void activateCanvasOnClic(GameObject o)
+    {
+        o.SetActive(true);
+    }
+}
